0fcdb85750aea8c5f63bf54b645ed69b09a5fcd9
jijiezhou <jijiez@email.unc.edu>
2023-09-20 23:27:32 -0400

    forEach and map

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  * @Author: ZJJ
  * @Date: 2023-09-20 10:31:53
  * @LastEditors: ZJJ
- * @LastEditTime: 2023-09-20 23:16:29
+ * @LastEditTime: 2023-09-20 23:23:35
  */
 
 const btn = document.querySelector(".btn-open");
