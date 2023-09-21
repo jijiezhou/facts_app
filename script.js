006e043827101196b47a76d3c5e9b9d07dbbc299
jijiezhou <jijiez@email.unc.edu>
2023-09-20 23:50:33 -0400

    create DOM elements

--- script.js
+++ script.js
@@ -4,14 +4,76 @@
  * @Author: ZJJ
  * @Date: 2023-09-20 10:31:53
  * @LastEditors: ZJJ
- * @LastEditTime: 2023-09-20 23:37:25
+ * @LastEditTime: 2023-09-20 23:49:44
  */
 
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+//Selecting DOM elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 const factsList = document.querySelector(".facts-list");
-factsList.innerHTML = "";
 
+//Create DOM elements: Render facts in list
+factsList.innerHTML = "";
+createFactsList(initialFacts);
+
+function createFactsList(dataArray) {
+  const htmlArr = dataArray.map(
+    (fact) => `
+  <li class="fact">
+              <p>
+                ${fact.text}
+                <a class="source" href="${fact.source}">(Source)</a>
+              </p>
+              <span class="tag" style="background-color: #3b82f6"
+                >${fact.category}</span
+              >
+              <div class="vote-buttons">
+                <button>👍 24</button>
+                <button>🤯 9</button>
+                <button>⛔️ 4</button>
+              </div>
+            </li>
+`
+  );
+  const html = htmlArr.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+//Toggle form visibility
 btn.addEventListener("click", () => {
   //console.log(form.classList);
   if (form.classList.contains("hidden")) {
