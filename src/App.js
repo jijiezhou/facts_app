044eea348ef72ec9385b79ad07c9643bd607f621
jijiezhou <jijiez@email.unc.edu>
2023-09-21 22:41:41 -0400

    jsx

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@
  * @Author: ZJJ
  * @Date: 2023-09-21 22:22:18
  * @LastEditors: ZJJ
- * @LastEditTime: 2023-09-21 22:24:29
+ * @LastEditTime: 2023-09-21 22:38:05
  */
+import "./style.css";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I learned logo" />
+        <h1>Today I learned</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
 }
 
 export default App;
