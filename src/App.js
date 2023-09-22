eba571cbd4743ee1dfb6256c0eac8c1c7725db2e
jijiezhou <jijiez@email.unc.edu>
2023-09-21 22:30:03 -0400

    setting up React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion: ZJJ Code
+ * @version: 1.0.0
+ * @Author: ZJJ
+ * @Date: 2023-09-21 22:22:18
+ * @LastEditors: ZJJ
+ * @LastEditTime: 2023-09-21 22:24:29
+ */
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Descripttion: ZJJ Code
+ * @version: 1.0.0
+ * @Author: ZJJ
+ * @Date: 2023-09-21 22:22:26
+ * @LastEditors: ZJJ
+ * @LastEditTime: 2023-09-21 22:22:52
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
